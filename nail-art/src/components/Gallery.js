a2fd7bb7be81872733f619ab5b71c1be9c930830
Scarletbobcat <tien.hoang314@yahoo.com>
2023-06-07 16:21:45 -0400

    added components folder

---
+++ nail-art/src/components/Gallery.js
@@ -0,0 +1,11 @@
+const Gallery = () => {
+    return ( 
+        <div className="gallery">
+            <img src="../images/nails_1.jpeg" alt="Nails_1" className="nails_1"/>
+            <img src="../images/nails_2.jpeg" alt="Nails_1" className="nails_2"/>
+            <img src="../images/nails_3.png" alt="Nails_1" className="nails_3"/>
+        </div>
+     );
+}
+ 
+export default Gallery;
--- nail-art/src/pages/Home.js
+++ nail-art/src/pages/Home.js
@@ -1,16 +1,10 @@
-import nails_1 from '../images/nails_1.jpeg'
-import nails_2 from '../images/nails_2.jpeg'
-import nails_3 from '../images/nails_3.png'
+import Gallery from "../components/Gallery";
 
 const Home = () => {
     return ( 
         <div className="home">
             <h1 className="gallery-title">Gallery</h1>
-            <div className="gallery">
-                <img src={nails_1} alt="Nails_1" className="nails_1"/>
-                <img src={nails_2} alt="Nails_2" className="nails_2"/>
-                <img src={nails_3} alt="Nails_3" className="nails_3"/>
-            </div>
+            < Gallery />
         </div>
     );
 }
